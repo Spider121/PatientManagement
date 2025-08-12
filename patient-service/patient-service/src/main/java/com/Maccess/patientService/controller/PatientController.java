e5b977b45fcb4a4b9cea5fd005930a54df7dbcd6
premM <premmankar804@gmail.com>
2025-08-12 12:18:41 +0530

    Added Http methods

--- patient-service/patient-service/src/main/java/com/Maccess/patientService/controller/PatientController.java
+++ patient-service/patient-service/src/main/java/com/Maccess/patientService/controller/PatientController.java
@@ -1,9 +1,25 @@
 package com.Maccess.patientService.controller;
 
+import com.Maccess.patientService.dto.PatientResponseDto;
+import com.Maccess.patientService.service.PatientService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/getPatients")
+    public List<PatientResponseDto> getAllPatients(){
+      return  patientService.getPatients();
+    }
 }
